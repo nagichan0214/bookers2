5db807054dfac6e7d692526ce5a3a5e989f18127
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-01-21 12:26:28 +0000

    [finish]35

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
-  def new
+
+
+  def create
     @book = Book.new
+    @book.save
+    redirect_to books_path(current_user.id)
+  end
+
+  def index
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+ devise_for :users
  root to: 'homes#top'
-  resources :users, only: [:show, :edit]
-  get 'books/new'
+ get 'homes/about' => 'homes#about'
+ resources :books,only: [:show, :edit, :index, :create, :new]
+ resources :users, only: [:show, :edit, :index]
+
+
   #get 'homes/top'
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
